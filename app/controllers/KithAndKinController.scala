2878fa6a9646299d3120a02c4dd35aada0eb590d
Simon Hildrew <simon.hildrew@theguardian.com>
2017-02-02 01:44:38 +0000

    Cache static pages for a few minutes

--- app/controllers/KithAndKinController.scala
+++ app/controllers/KithAndKinController.scala
@@ -3,19 +3,21 @@ package controllers
 import play.api.mvc.{Action, Controller}
 
 class KithAndKinController extends Controller {
+  val fifteenMinuteCache = CACHE_CONTROL -> "public; max-age=900"
+
   def healthcheck = Action {
     Ok("OK")
   }
 
   def index = Action {
-    Ok(views.html.index())
+    Ok(views.html.index()).withHeaders(fifteenMinuteCache)
   }
 
   def accommodation = Action {
-    Ok(views.html.accommodation())
+    Ok(views.html.accommodation()).withHeaders(fifteenMinuteCache)
   }
 
   def getInvolved = Action {
-    Ok(views.html.getInvolved())
+    Ok(views.html.getInvolved()).withHeaders(fifteenMinuteCache)
   }
 }
